75c99c1f3d577ed6b8120e318fe7c7dcd965a978
Teta_Lena <tetalenaa@gmail.com>
2023-06-02 12:03:13 +0200

    home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,9 @@ const HomePage = () => {
     <div className="home-container">
       <div className="header">
         <div className="top-header">
-          <div className="user"></div>
+          <div className="user">
+            <p>Jacques Kagabo</p>
+          </div>
           <div className="title">
             <h2>
               Supa<span>Menu</span>
@@ -13,13 +15,32 @@ const HomePage = () => {
           </div>
         </div>
         <div className="menu-header">
-          <p>motto</p>
+          <p className="menu-header-p">Register your restaurant on SupaMenu</p>
+          <p className="sub-menu-header">for free and get more revenue</p>
           <div className="options-btns">
-            <p>btns</p>
+            <button>Register your restaurant</button>
+            <button> Restaurant already registered? Sign in </button>
           </div>
         </div>
       </div>
-      <div className="options"></div>
+
+      <div className="options">
+        <p className="options-p">How it works</p>
+        <div className="boxes">
+          <div className="box-step">
+            <h4>Step 1</h4>
+            <p>Register your restaurant</p>
+          </div>
+          <div className="box-step">
+            <h4>Step 2</h4>
+            <p>Create your restaurant profile and create menu items</p>
+          </div>
+          <div className="box-step">
+            <h4>Step 3</h4>
+            <p>Start receiving orders</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
