ef7cd6d0dc081a1fdd22fb381d2951f4cc9d971c
Teta_Lena <tetalenaa@gmail.com>
2023-06-02 09:55:25 +0200

    init files and homepage

---
+++ src/components/HomePage.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="home-container">
+      <div className="header">
+        <div className="top-header">
+          <div className="user"></div>
+          <div className="title">
+            <h2>
+              Supa<span>Menu</span>
+            </h2>
+          </div>
+        </div>
+        <div className="menu-header">
+          <p>motto</p>
+          <div className="options-btns">
+            <p>btns</p>
+          </div>
+        </div>
+      </div>
+      <div className="options"></div>
+    </div>
+  );
+};
+
+export default HomePage;
