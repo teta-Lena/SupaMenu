09fe4450f36d1befffd6c9019190190a124de2e7
Teta_Lena <tetalenaa@gmail.com>
2023-06-05 23:54:53 +0200

    dashboard done

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAdd, faChartPie, faPeopleGroup, faScrewdriverWrench, faTicket, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Dashboard = () => {
   return (
@@ -8,15 +10,19 @@ const Dashboard = () => {
         <p><span>Supa</span>Menu</p>
         <nav>
             <ul>
-                <li>Overview</li>
-                <li>Clients</li>
-                <li>Users</li>
+            
+               <li><FontAwesomeIcon icon={faChartPie} style={{padding:"0px 10px"}}/>Overview</li>
+            
+                <li><FontAwesomeIcon icon={faTicket} style={{padding:"0px 10px"}}/>Clients</li>
+          
+                <li><FontAwesomeIcon icon={faPeopleGroup} style={{padding:"0px 10px"}}/>Users</li>
+            
             </ul>
         </nav>
 
         <ul>
-             <li>Settings</li>
-             <li>My Account</li>
+             <li><FontAwesomeIcon icon={faScrewdriverWrench} style={{padding:"0px 5px"}}/>Settings</li>
+             <li><FontAwesomeIcon icon={faUser} style={{padding:"0px 5px"}}/>My Account</li>
         </ul>
         </div>
        <div className='nav-orange'>
@@ -39,17 +45,130 @@ const Dashboard = () => {
         </div>
         <div className='dash-graph'></div>
         <div className='dash-data-details'>
+          
             <div className='details-1'>
-                <div></div>
-                <div></div>
+           
+
+            <div className="detailed-stats">
+             <div>
+              <h4>Restaurant</h4>
+              <p style={{color:"#f49e06", padding:12}}>View details</p>
+             </div>
+             <div>
+              <h6>sales</h6>
+             </div>
+             <div>
+              <h5>Sole luna</h5>
+              <p>46000</p>
+             </div>
+             <div>
+              <h5>Soy</h5>
+              <p>14000</p>
+             </div>
             </div>
+            <div className="detailed-stats">
+            <div>
+              <h4>Restaurant</h4>
+              <p style={{color:"#f49e06", padding:12}}>View details</p>
+             </div>
+             <div>
+              <h6>sales</h6>
+             </div>
+             <div>
+              <h5>Sole luna</h5>
+              <p>46000</p>
+             </div>
+             <div>
+              <h5>Soy</h5>
+              <p>14000</p>
+             </div>
+
+
+</div>
+            
+</div>
             <div className='details-2'>
-                <div></div>
-                <div></div>
+            <div className="detailed-stats">
+             <div>
+              <h4>Restaurant</h4>
+              <p style={{color:"#f49e06", padding:12}}>View details</p>
+             </div>
+             <div>
+              <h6>sales</h6>
+             </div>
+             <div>
+              <h5>Sole luna</h5>
+              <p>46000</p>
+             </div>
+             <div>
+              <h5>Soy</h5>
+              <p>14000</p>
+             </div>
+            </div>
+            <div className="detailed-stats">
+            <div>
+              <h4>Restaurant</h4>
+              <p style={{color:"#f49e06", padding:12}}>View details</p>
+             </div>
+             <div>
+              <h6>sales</h6>
+             </div>
+             <div>
+              <h5>Sole luna</h5>
+              <p>46000</p>
+             </div>
+             <div>
+              <h5>Soy</h5>
+              <p>14000</p>
+             </div>
+</div>
             </div>
             <div className='details-3'>
+            <div className="detailed-stats">
+             <div className='content'>
+              <h4>Create</h4>
+              <p style={{color:"#f49e06", padding:12}}>View All</p>
+             </div>
+             <div className='content'>
+              <h6>Today</h6>
+             </div>
+             <div className='content'>
+              <p style={{fontSize:"15px"}}>Create new</p>
+              <FontAwesomeIcon icon={faAdd}/>
+             </div>
+             <div className='line-3'></div>
+             <div className="content">
+              <form>
+            <div className="dash-form">
+             <div className='c3-form'>
+              <div className='content-3'>
+                <input type="checkbox" name="restaurant" />
+               <p>Restaurant</p>
+                </div>
+                  <span className='dash-span'>New</span>
+                  </div>
 
-            </div>
+                  <div className='c3-form'>
+              <div className='content-3'>
+                <input type="checkbox" name="restaurant" />
+               <p>Restaurant</p>
+                </div>
+                  <span className='dash-span'>New</span>
+                  </div>
+
+                  <div className='c3-form'>
+              <div className='content-3'>
+                <input type="checkbox" name="restaurant" />
+               <p>Restaurant</p>
+                </div>
+                  <span className='dash-span'>New</span>
+                  </div>
+ </div>
+             </form>
+              
+             </div>
+        </div>
+        </div>
         </div>
       </div>
     </div>
